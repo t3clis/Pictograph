fa376497cc5270094707ecc0eaed94dd23d86097
Alessio Massuoli <alessio.massuoli@quixant.com>
2017-08-21 16:25:57 +0200

    tiled floor and snapshot functions added

--- Pictograph/MainWindow.xaml.cs
+++ Pictograph/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,17 +21,76 @@ namespace Pictograph
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string DEFAULT_DIR = "Pictograph_";
+        private const string DEFAULT_FILE = "pg";
+        public string SaveDirectory { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            SaveDirectory = GetSaveDirectory();
             Icon = new BitmapImage(new Uri("Resources/Pictograph.png", UriKind.Relative));
             floorScaleSlider.MouseDoubleClick += new MouseButtonEventHandler(RestoreScalingFactor);
+        }
+
+        private string GetSaveDirectory()
+        {
+            string baseDir = Directory.GetCurrentDirectory();
+            int i = 0;
+
+            do
+            {
+                string dir = baseDir + "\\" + DEFAULT_DIR + i++.ToString().PadLeft(2, '0') + "\\";
+
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                    return dir;
+                }
+            } while (i < 100);
+
+            throw new InvalidOperationException("Cannot find an available directory");
         }
 
         void RestoreScalingFactor(object sender, MouseButtonEventArgs args)
         {
 
             ((Slider)sender).Value = 1.0;
+        }
+
+        private void Snap_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = GetFilePath();
+
+            RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap((int)sViewport.ActualWidth, (int)sViewport.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+            renderTargetBitmap.Render(gViewport);
+            PngBitmapEncoder pngImage = new PngBitmapEncoder();
+            pngImage.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+            using (Stream fileStream = File.Create(filePath))
+            {
+                pngImage.Save(fileStream);
+            }
+
+            tSnapResult.Text = System.IO.Path.GetFileName(filePath) + " saved.";
+        }
+
+        private string GetFilePath()
+        {
+            string file_prefix = SaveDirectory + DEFAULT_FILE;
+            int i = 0;
+
+            do
+            {
+                string fn = file_prefix +  i++.ToString().PadLeft(3, '0') + ".png";
+
+                if (!File.Exists(fn))
+                {
+                    return fn;
+                }
+
+            } while (i < 1000);
+
+            throw new InvalidOperationException("Cannot find an available file name");
         }
     }
 }
---
+++ PictographControls/TiledButton.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PictographControls
+{
+    public enum TiledButtonState
+    {
+        Base,
+        Move,
+        Dash,
+        BlindSpot,
+        SolidObject,
+        Range
+    }
+
+    public partial class TiledButton : UserControl
+    {
+        public TiledButtonState TileState { get; set; }
+
+        public TiledButton()
+        {
+            InitializeComponent();
+            TileState = TiledButtonState.Base;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            switch (TileState)
+            {
+                case TiledButtonState.Base:
+                    {
+                        b.Style = (Style)FindResource("styleMove");
+                        TileState = TiledButtonState.Move;
+                    }
+                    break;
+                case TiledButtonState.Move:
+                    {
+                        b.Style = (Style)FindResource("styleDash");
+                        TileState = TiledButtonState.Dash;
+                    }
+                    break;
+                case TiledButtonState.Dash:
+                    {
+                        b.Style = (Style)FindResource("styleBlindSpot");
+                        TileState = TiledButtonState.BlindSpot;
+                    }
+                    break;
+                case TiledButtonState.BlindSpot:
+                    {
+                        b.Style = (Style)FindResource("styleSolidObject");
+                        TileState = TiledButtonState.SolidObject;
+                    }
+                    break;
+                case TiledButtonState.SolidObject:
+                    {
+                        b.Style = (Style)FindResource("styleRange");
+                        TileState = TiledButtonState.Range;
+                    }
+                    break;
+                case TiledButtonState.Range:
+                default:
+                    {
+                        b.Style = (Style)FindResource("styleBase");
+                        TileState = TiledButtonState.Base;
+                    }
+                    break;
+            }
+        }
+
+        private void Button_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button b = sender as Button;
+            b.Style = (Style)FindResource("styleBase");
+            TileState = TiledButtonState.Base;
+        }
+    }
+}
