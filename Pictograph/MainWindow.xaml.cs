25c3926e9d34a1107046b7e239bfcfe1ec04731f
Alessio Massuoli <alessio.massuoli@quixant.com>
2017-08-22 16:47:33 +0200

    Added drag and drop (basic) screenshot to F2 Added interactivity to survivor tokens

--- Pictograph/MainWindow.xaml.cs
+++ Pictograph/MainWindow.xaml.cs
@@ -14,16 +14,17 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using PictographControls;
+using System.Timers;
 
 namespace Pictograph
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private const string DEFAULT_DIR = "Pictograph_";
+        private const string DEFAULT_DIR = "Pictograph Snapshots";
         private const string DEFAULT_FILE = "pg";
+
+        private Point _toolDragStart;
+
         public string SaveDirectory { get; set; }
 
         public MainWindow()
@@ -40,26 +41,19 @@ namespace Pictograph
                     Canvas.SetTop(t, j * 100);
                     gViewport.Children.Add(t);
                 }
+
             floorScaleSlider.MouseDoubleClick += new MouseButtonEventHandler(RestoreScalingFactor);
         }
 
         private string GetSaveDirectory()
         {
             string baseDir = Directory.GetCurrentDirectory();
-            int i = 0;
+            string dir = baseDir + "\\" + DEFAULT_DIR + "\\";
 
-            do
-            {
-                string dir = baseDir + "\\" + DEFAULT_DIR + i++.ToString().PadLeft(2, '0') + "\\";
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
 
-                if (!Directory.Exists(dir))
-                {
-                    Directory.CreateDirectory(dir);
-                    return dir;
-                }
-            } while (i < 100);
-
-            throw new InvalidOperationException("Cannot find an available directory");
+            return dir;
         }
 
         void RestoreScalingFactor(object sender, MouseButtonEventArgs args)
@@ -102,5 +96,114 @@ namespace Pictograph
 
             throw new InvalidOperationException("Cannot find an available file name");
         }
+
+        private void listTools_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Store the mouse position
+            _toolDragStart = e.GetPosition(null);
+        }
+
+        private void listTools_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            // Get the current mouse position
+            Point mousePos = e.GetPosition(null);
+            Vector diff = _toolDragStart - mousePos;
+            string itemFormat = null;
+            object item = null;
+
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+                // Get the dragged ListViewItem
+                ListView listView = sender as ListView;
+                ListViewItem listViewItem = (ListViewItem)listView.SelectedItem;
+
+                if (liSurvivor.IsSelected)
+                {
+                    itemFormat = "survivor";
+                    item = new SurvivorToken();
+                }
+
+                if (item != null)
+                {
+                    // Initialize the drag & drop operation
+                    DataObject dragData = new DataObject(itemFormat, item);
+                    DragDrop.DoDragDrop(listViewItem, dragData, DragDropEffects.Move);
+                }
+            }
+        }
+
+        private void gViewport_Drop(object sender, DragEventArgs e)
+        {
+            Point dropPoint = e.GetPosition(gViewport);
+
+            if (e.Data.GetDataPresent("survivor"))
+            {
+                int top, left;
+                SurvivorToken item = e.Data.GetData("survivor") as SurvivorToken;
+
+                top = (((int)dropPoint.Y) / 100) * 100;
+                left = (((int)dropPoint.X) / 100) * 100;
+
+                Canvas.SetTop(item, top);
+                Canvas.SetLeft(item, left);
+                gViewport.Children.Add(item);
+            }
+        }
+
+        private void gViewport_DragEnter(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent("survivor") || sender == e.Source)
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F2)
+                Snap_Click(this, e);
+        }
+
+        /*
+         *         private void CardList_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+        {
+            // update the position of the visual feedback item
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+
+            this._dragdropWindow.Left = w32Mouse.X;
+            this._dragdropWindow.Top = w32Mouse.Y;
+        }
+
+        private void CreateDragDropWindow(Visual dragElement)
+        {
+            this._dragdropWindow = new Window();
+            _dragdropWindow.WindowStyle = WindowStyle.None;
+            _dragdropWindow.AllowsTransparency = true;
+            _dragdropWindow.AllowDrop = false;
+            _dragdropWindow.Background = null;
+            _dragdropWindow.IsHitTestVisible = false;
+            _dragdropWindow.SizeToContent = SizeToContent.WidthAndHeight;
+            _dragdropWindow.Topmost = true;
+            _dragdropWindow.ShowInTaskbar = false;
+
+            Rectangle r = new Rectangle();
+            r.Width = ((FrameworkElement)dragElement).ActualWidth;
+            r.Height = ((FrameworkElement)dragElement).ActualHeight;
+            r.Fill = new VisualBrush(dragElement);
+            this._dragdropWindow.Content = r;
+
+
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+
+
+            this._dragdropWindow.Left = w32Mouse.X;
+            this._dragdropWindow.Top = w32Mouse.Y;
+            this._dragdropWindow.Show();
+        }
+         * */
     }
 }
